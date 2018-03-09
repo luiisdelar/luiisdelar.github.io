99a82f60a2f4299746c02df14d471aedbf1b1872
Luis <luiseduardo.ortega@unet.edu.ve>
2018-03-08 22:22:35 -0400

    Todo en otra ruta

--- calculadoraUNET/js/logic.js
+++ calculadoraUNET/js/logic.js
@@ -3,7 +3,7 @@ function aceptar() {
 		var select = parseInt(document.getElementById('seleccion').value);
 		var capa = document.getElementById("flujo");
 		var elemento = document.getElementById("botonaceptar");
-		var input, label, label2, div;
+		var input, input2, label, label2, div;
 		var x = elemento.parentNode;
 		var volver = document.createElement("button");
 		var calcular = document.createElement("button");
@@ -26,14 +26,22 @@ function aceptar() {
 			div = document.createElement('div'); 
 			input = document.createElement("input");
 			label = document.createElement("label");
+			input2 = document.createElement("input");
+			label2 = document.createElement("label");
 			
+
 			input.setAttribute("id","input"+(i+1));
 			input.className = "form-control";
+			input2.setAttribute("id","input"+(i+1));
+			input2.className = "form-control";
 			div.className = "form-group";
 			label.innerHTML = "Parcial "+(i+1);
-			
+			label2.innerHTML = "Porcentaje ";
+
 			div.appendChild(label);
-			div.appendChild(input);			
+			div.appendChild(input);
+			div.appendChild(label2);
+			div.appendChild(input2);				
 			capa.appendChild(div);
 		
 		}
